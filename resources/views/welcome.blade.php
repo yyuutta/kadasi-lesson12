f0935e19c81a178b73695d30707098b9d2027d3a
yyuutta <ayaragi.yamayu@gmail.com>
2018-07-22 04:05:51 +0000

    timeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,23 @@
 
 @section('content')
     @if (Auth::check())
-        <?php $user = Auth::user(); ?>
-        {{ $user->name }}
+        <div class="row">
+            <aside class="col-xs-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">{{ $user->name }}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
+                    </div>
+                </div>
+            </aside>
+            <div class="col-xs-8">
+                @if (count($microposts) > 0)
+                    @include('microposts.microposts', ['microposts' => $microposts])
+                @endif
+            </div>
+        </div>
     @else
         <div class="center jumbotron">
             <div class="text-center">
